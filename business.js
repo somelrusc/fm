98c7efcb71f4d0a6d0d02b851de625595370c3e2
somelrusc <somelrusc@gmail.com>
2026-09-14 19:13:08 +0200

    Add files via upload

---
+++ business.js
@@ -0,0 +1,53 @@
+const organizersData = {
+    "Ajuntament de Molins de Rei": { 
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbgPLm783O4TdWmu8hI6AXaAj5nh1q7mkBeUvgZVRa1bI5XziVQOlzm5M&s=10", 
+        verified: true, 
+        badgeType: "blue",
+        verifiedDesc: "Institució pública",
+        desc: "Activitats Organitzades per l'Ajuntament de Molins de Rei",
+        ig: "https://www.instagram.com/ajuntamentmolinsderei/",
+        linkedOrgs: ["Cultura Molins"]
+    },
+    "Esplai Molins": { 
+        img: "", 
+        verified: false, 
+        badgeType: "",
+        verifiedDesc: "",
+        desc: "Entitat d'educació en el lleure per a infants i joves de la vila.",
+        ig: "https://instagram.com/esplaimolins",
+        linkedOrgs: []
+    },
+    "Cultura Molins": { 
+        img: "hhttps://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbgPLm783O4TdWmu8hI6AXaAj5nh1q7mkBeUvgZVRa1bI5XziVQOlzm5M&s=10", 
+        verified: true, 
+        badgeType: "Institució pública",
+        verifiedDesc: "",
+        desc: "Departament de cultura de l'Ajuntament de Molins de Rei.",
+        ig: "https://www.instagram.com/culturamolins/",
+        linkedOrgs: ["Ajuntament de Molins de Rei"]
+    },
+    "Diables de Molins de Rei": { 
+        img: "https://images.unsplash.com/photo-1604928141064-207cea6f5722?auto=format&fit=crop&w=200&q=80", 
+        verified: false, 
+        badgeType: "",
+        verifiedDesc: "",
+        desc: "Colla històrica de diables i tabalers.",
+        ig: "https://instagram.com/diablesmolins",
+        linkedOrgs: ["Amics del Camell"]
+    },
+    "Amics del Camell": { 
+        img: "https://yt3.googleusercontent.com/GMJd-XXeCRC2_Ja6ObGlBszZlHMmUGNCDM6-ogKT1kNPYvawJWmo1W2968c9UFkOOTdFcmF7=s900-c-k-c0x00ffffff-no-rj", 
+        verified: false, 
+        badgeType: "",
+        verifiedDesc: "",
+        desc: "Sóc una bèstia de foc de Molins de Rei recuperada l'any 1981 d'una tradició molinenca del Carnaval del segle XVIII.",
+        linkedOrgs: ["Diables de Molins de Rei"]
+    },
+    "BeDisco": { 
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqJfkrEMTw0nlBHU0joDm9TzcSY6AGctk5EPA8XltwFw&s", 
+        verified: false, 
+        badgeType: "",
+        verifiedDesc: "",
+        desc: "Discoteca BeDisco.",
+    },
+};
