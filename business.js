b336c42553468f722e635430a334cf41fb7972a0
somelrusc <somelrusc@gmail.com>
2026-09-15 16:10:13 +0200

    Update business.js

--- business.js
+++ business.js
@@ -18,10 +18,10 @@ const organizersData = {
         linkedOrgs: []
     },
     "Cultura Molins": { 
-        img: "hhttps://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbgPLm783O4TdWmu8hI6AXaAj5nh1q7mkBeUvgZVRa1bI5XziVQOlzm5M&s=10", 
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbgPLm783O4TdWmu8hI6AXaAj5nh1q7mkBeUvgZVRa1bI5XziVQOlzm5M&s=10", 
         verified: true, 
-        badgeType: "Institució pública",
-        verifiedDesc: "",
+        badgeType: "",
+        verifiedDesc: "Institució pública",
         desc: "Departament de cultura de l'Ajuntament de Molins de Rei.",
         ig: "https://www.instagram.com/culturamolins/",
         linkedOrgs: ["Ajuntament de Molins de Rei"]
@@ -43,11 +43,44 @@ const organizersData = {
         desc: "Sóc una bèstia de foc de Molins de Rei recuperada l'any 1981 d'una tradició molinenca del Carnaval del segle XVIII.",
         linkedOrgs: ["Diables de Molins de Rei"]
     },
-    "BeDisco": { 
-        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqJfkrEMTw0nlBHU0joDm9TzcSY6AGctk5EPA8XltwFw&s", 
+    "One Mile Crossfit": { 
+        img: "https://onemilecrossfit.com/wp-content/uploads/2025/07/logo_black.png", 
         verified: false, 
         badgeType: "",
         verifiedDesc: "",
-        desc: "Discoteca BeDisco.",
+        ig: "https://www.instagram.com/onemilecrossfit/",
+        desc: "One Mile Crossfit Molins.",
+    },
+    "Esports Molins": { 
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJ1xsZDgriuzw1dII5uQLsGNhFIJOs37d91dkA56Vh_Q&s", 
+        verified: true, 
+        badgeType: "",
+        verifiedDesc: "Institució Pública",
+        ig: "https://www.instagram.com/esports_molins/",
+        desc: "Regidoria d'Esports de Molins de Rei.",
+    },
+    "Foment Cultural i Artístic": { 
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6tp2YqExLoXhl_pKK5__PXDAY2U5zFBvabAdEaXNml9X_--gCPJgoJO4Q&s=10", 
+        verified: false, 
+        badgeType: "",
+        verifiedDesc: "",
+        ig: "https://www.instagram.com/fomentmolins/",
+        desc: "Som una entitat centenària sense ànim de lucre que té com a finalitat la promoció i el foment de la cultura en tots els seus registres 🎭.",
+    },
+     "Museu del Renaixement": { 
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9lmCTCKmomIZRjYv3So3YAX1hXKMjGNX5_MSWBW5-FK5k-jfM1PbXmFI&s=10", 
+        verified: true, 
+        badgeType: "",
+        verifiedDesc: "Institució Pública",
+        ig: "https://www.instagram.com/museurenaixement/",
+        desc: "🏛️ El Museu del Renaixement explica l'art, la història, la cultura i la societat del s. XVI. 📌 Ubicat al Palau de Requesens, Molins de Rei",
+    },
+    "Grup del Drac de Molins de Rei el Cuc": { 
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ82FoYQCa4N8fpjFILwvtFR8vICurvp3-j5Uksj1n3YqOJ3u8YqVfk-uhq&s=10", 
+        verified: false, 
+        badgeType: "",
+        verifiedDesc: "",
+        ig: "https://www.instagram.com/el_cuc_molins_de_rei/",
+        desc: "Grup del drac de molins de rei format l'any 1995.",
     },
 };
